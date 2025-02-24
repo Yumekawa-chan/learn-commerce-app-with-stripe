44e8d547461ed16b6bd5efaf5bc08c210ac1e951
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2025-02-25 00:04:05 +0900

    update

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -4,8 +4,18 @@ import initiStripe from 'stripe';
 import { cookies } from 'next/headers';
 
 export async function POST(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get('API_ROUTE_SECRET');
+  if (query !== process.env.API_ROUTE_SECRET) {
+    return NextResponse.json(
+      {
+        message: 'Invalid secret',
+      },
+      { status: 401 },
+    );
+  }
+
   const data = await req.json();
-  const { id, email } = data;
+  const { id, email } = data.record;
   const supabase = createRouteHandlerClient({ cookies });
 
   const stripe = new initiStripe(process.env.STRIPE_SECRET_KEY!);
