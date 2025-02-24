2d1877e23457e28f3c1f9af7d61fa8cad34ff7fd
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2025-02-24 16:00:45 +0900

    update

--- components/auth/AuthClientButton.tsx
+++ components/auth/AuthClientButton.tsx
@@ -1,16 +1,39 @@
 'use client';
-import { Session } from '@supabase/auth-helpers-nextjs';
+
+import {
+  Session,
+  createClientComponentClient,
+} from '@supabase/auth-helpers-nextjs';
 import { Button } from '../ui/button';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+
 const AuthClientButton = ({ session }: { session: Session | null }) => {
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
   const handleSignin = () => {
-    const supabase = createClientComponentClient();
     supabase.auth.signInWithOAuth({
       provider: 'github',
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
     });
   };
+  const handleSignout = () => {
+    const supabase = createClientComponentClient();
+    supabase.auth.signOut();
+    router.refresh();
+  };
 
-  return <Button onClick={handleSignin}>サインイン</Button>;
+  return (
+    <>
+      {session ? (
+        <Button onClick={handleSignout}>ログアウト</Button>
+      ) : (
+        <Button onClick={handleSignin}>サインイン</Button>
+      )}
+    </>
+  );
 };
 
 export default AuthClientButton;
